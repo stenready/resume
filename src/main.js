4f33577dceb2fb1730cf671c3527b017f93ae01a
Sten Ready <Sten Ready>
2025-07-01 02:17:52 +0300

    fix(restore normal imports): v1

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import './assets/main.scss'
 
 //icons
-import { FontAwesomeIcon } from '/@fortawesome/vue-fontawesome'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 ///
 
-import { i18n } from '/plugins/i18n.js'
-import '/plugins/icons.js'
+import { i18n } from '@/plugins/i18n.js'
+import '@/plugins/icons.js'
 
 import { createApp } from 'vue'
 import App from './App.vue'
