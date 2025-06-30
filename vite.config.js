22ae37616ad7c71a90158580af0ae340feb565b7
Sten Ready <Sten Ready>
2025-07-01 02:22:17 +0300

    fix change with path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import svgLoader from 'vite-svg-loader'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -54,7 +55,7 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 })
